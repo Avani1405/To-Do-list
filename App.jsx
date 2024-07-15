991e26d0abebb1f58440b6e930c55f8ffbfa476e
Avani1405 <145357470+Avani1405@users.noreply.github.com>
2024-07-15 22:02:59 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ToDo from './Components/ToDo'
+
+function App() {
+  return (
+    <div className='bg-stone-600 grid py-4 min-h-screen'>
+     <ToDo />
+    </div>
+  )
+}
+
+export default App
